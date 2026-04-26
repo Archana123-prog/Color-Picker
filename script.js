c0371461c9ca89c139615391838e6be47319bde4
Archana123-prog <archana12f@gmail.com>
2026-04-26 23:57:45 +0530

    Color Picker

---
+++ script.js
@@ -0,0 +1,19 @@
+function generateColor() {
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  document.getElementById("colorBox").style.background = color;
+  document.getElementById("colorCode").innerText = color;
+}
+
+function copyColor() {
+  let color = document.getElementById("colorCode").innerText;
+
+  navigator.clipboard.writeText(color);
+
+  alert("Copied: " + color);
+}
